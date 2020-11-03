f3f8010bb1cfd0e8c3f4339110eb7dd34e3f5a0b
Jehyun <congmul@hotmail.com>
2020-11-02 20:20:15 -0800

    Add if statment for running forever until selecting EXIT

--- index.js
+++ index.js
@@ -26,8 +26,36 @@ inquirer
         pet = new digitalPal(response.petName, "cat");
     }
 
-    
-
+        play();
+        
   });
 
 
+function play(){
+    inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What do you want to do with your pet?",
+      name: "toDo",
+      choices: [
+          "feed",
+          "sleep",
+          "play",
+          "sound",
+          "goOutside",
+          "goInside",
+          "destroyFurniture",
+          "buyNewFurniture",
+          "EXIT"
+      ]
+    }
+  ]).then(function(response) {
+    console.log(response.toDo);
+    pet.sound();
+    
+    if(response.toDo !== "EXIT"){
+        play();
+    }
+  });
+}
