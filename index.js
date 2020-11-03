5a4d785e922162ab5b3fdc3fbd734cb1d7797821
Jehyun <congmul@hotmail.com>
2020-11-02 19:04:03 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+function DigitalPal(){
+    this.hungry = false;
+    this.spleepy = false;
+    this.bored = true;
+    this.age = 0;
+    this.outside = false;
+    this.houseCondition = 100;
+}
+
+DigitalPal.prototype = {
+    feed : function(){
+
+    },
+    sleep : function(){
+        
+    },
+    play : function(){
+        
+    },
+    increaseAge : function(){
+        
+    },
+    bark : function(){
+        
+    },
+    goOutside : function(){
+        
+    },
+    goInside : function(){
+        
+    },
+    meow : function(){
+        
+    },
+    destroyFurniture : function(){
+        
+    },
+    buyNewFurniture : function(){
+        
+    }
+}
+
+let dog = new DigitalPal();
+let cat = new DigitalPal();
+
+console.log(newOne);
